5f99fe39b55a0b4414aaf1a90e4ac668b9a3b27a
Costin Galan <cgalan@cloudbasesolutions.com>
2016-05-04 11:22:12 +0300

    Initial draft
    Signed-off-by: Costin Galan <cgalan@cloudbasesolutions.com>

---
+++ script.ps1
@@ -0,0 +1,44 @@
+function New-WindowsISO()
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory=$true)]
+        [string]$baseImagePath,
+        [parameter(Mandatory=$true)]
+        [string]$outputImageName,
+        [parameter(Mandatory=$true)]
+        [string]$workDir,
+        [parameter(Mandatory=$false)]
+        [string]$indexList=@(1,2)
+    )
+
+    PROCESS
+    {
+        try {
+        $driveLetter = (Mount-DiskImage $baseImagePath -PassThru | Get-Volume).DriveLetter
+        If (! Test-Path $workDir)
+          {
+              Remove-Item -Force -Recurse "${workDir}"
+          }
+        New-Item -Type Directory $workDir
+        pushd $workDir
+        New-Item -Type Directory ISO
+        New-Item -Type Directory Updates
+        Copy-Item -Recurse "${driveLetter}\*" "ISO"
+        foreach $index in $indexList {
+            $workIndex = Join-Path -Path $workDir -ChildPath "index-${index}"
+            New-Item -Type Directory $workIndex
+            Dism.exe /mount-wim /wimfile:${workDir}\ISO\sources\install.wim /mountdir:$workIndex /index:$index
+            foreach $update in $updates {
+                Add-WindowsPackage -Path ${workIndex}\sources\install.wim -PackagePath $update.msu
+            }
+            Dism.exe /unmount-wim /mountdir:$workIndex /commit
+        }
+        & C:\Users\Administrator\mkisofs.exe -o iso_name.iso -ldots -allow-lowercase -allow-multidot -l -publisher "Cloudbase Solutions" -J -r -V "DVD Label" "${workDir}\ISO"
+    } finally {
+      Write-Host "New-ISO failed"
+      Write-Host $_
+      Dismount-DiskImage $baseImagePath
+      }
+}
