000ee90c022b89548756457498de58ede4c553e6
Costin Galan <cgalan@cloudbasesolutions.com>
2016-05-27 12:48:09 +0300

    Added fixes with the updates dir
    Signed-off-by: Costin Galan <cgalan@cloudbasesolutions.com>

--- script.ps1
+++ script.ps1
@@ -9,6 +9,8 @@ function New-WindowsISO()
         [string]$outputImageName,
         [parameter(Mandatory=$true)]
         [string]$workDir,
+        [parameter(Mandatory=$true)]
+        [string]$updatesDir,
         [parameter(Mandatory=$false)]
         [string]$indexList=@(1,2)
     )
@@ -16,26 +18,24 @@ function New-WindowsISO()
     PROCESS
     {
         try {
-        $driveLetter = (Mount-DiskImage $baseImagePath -PassThru | Get-Volume).DriveLetter
-        If (! Test-Path $workDir)
-          {
-              Remove-Item -Force -Recurse "${workDir}"
-          }
-        New-Item -Type Directory $workDir
-        pushd $workDir
-        New-Item -Type Directory ISO
-        New-Item -Type Directory Updates
-        Copy-Item -Recurse "${driveLetter}\*" "ISO"
-        foreach $index in $indexList {
-            $workIndex = Join-Path -Path $workDir -ChildPath "index-${index}"
-            New-Item -Type Directory $workIndex
-            Dism.exe /mount-wim /wimfile:${workDir}\ISO\sources\install.wim /mountdir:$workIndex /index:$index
-            foreach $update in $updates {
-                Add-WindowsPackage -Path ${workIndex}\sources\install.wim -PackagePath $update.msu
+            $driveLetter = (Mount-DiskImage $baseImagePath -PassThru | Get-Volume).DriveLetter
+            if (! Test-Path $workDir) {
+                Remove-Item -Force -Recurse "${workDir}"
             }
-            Dism.exe /unmount-wim /mountdir:$workIndex /commit
-        }
-        & C:\Users\Administrator\mkisofs.exe -o iso_name.iso -ldots -allow-lowercase -allow-multidot -l -publisher "Cloudbase Solutions" -J -r -V "DVD Label" "${workDir}\ISO"
+            New-Item -Type Directory $workDir
+            pushd $workDir
+            New-Item -Type Directory ISO
+            Copy-Item -Recurse "${driveLetter}\*" "ISO"
+            foreach $index in $indexList {
+                $workIndex = Join-Path -Path $workDir -ChildPath "index-${index}"
+                New-Item -Type Directory $workIndex
+                Dism.exe /mount-wim /wimfile:${workDir}\ISO\sources\install.wim /mountdir:$workIndex /index:$index
+                foreach $update in $updatesDir {
+                    Add-WindowsPackage -Path ${workIndex}\sources\install.wim -PackagePath $update
+                }
+                Dism.exe /unmount-wim /mountdir:$workIndex /commit
+            }
+            & C:\Users\Administrator\mkisofs.exe -o iso_name.iso -ldots -allow-lowercase -allow-multidot -l -publisher "Cloudbase Solutions" -J -r -V "DVD Label" "${workDir}\ISO"
     } finally {
       Write-Host "New-ISO failed"
       Write-Host $_
